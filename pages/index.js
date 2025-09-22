ae903578658be54b275f97882583e0b951765ef2
Vinay-Narasimhan1 <vinayn2020@gmail.com>
2025-09-22 12:21:13 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'system-ui, sans-serif', padding: '2rem' }}>
+      <h1>My Amplify Storefront</h1>
+      <p>🚀 Deployed with Next.js 13 static export.</p>
+      <p>This is a minimal starter. We'll add products next.</p>
+    </main>
+  );
+}
